ec95ba6140ad04bfb38def7201b3a2d79439be1f
Neil Haggerty <neil.haggerty@mergermarket.com>
2018-09-13 16:54:46 +0100

    Testing WIP

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,24 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_docker_installed(host):
+    assert host.package("docker").is_installed
+
+
+def test_pip_installed(host):
+    packages = host.pip_package.get_packages()
+    print(packages)
+    assert False
---
+++ verify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run --rm -it \
+    -v $(pwd):/ansible-role-docker:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -w /ansible-role-docker \
+        retr0h/molecule:latest \
+            # sudo molecule prepare && \
+            sudo molecule converge && \
+            sudo molecule verify
